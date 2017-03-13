1886870c3cb603b82a95714e9ce0ec91e4ffe2f9
Arnelle Balane <arnellebalane@gmail.com>
2017-03-13 15:01:14 +0800

    add places as sidebar list inside sidebar panel

---
+++ source/sidebar-list.vue
@@ -0,0 +1,50 @@
+<template>
+    <section class="sidebar-list">
+        <h1 class="list-title">{{title}}</h1>
+        <nav class="list-items">
+            <a class="list-item" :href="item.path" v-for="item in items">
+                <i class="icon icon--directory"></i>
+                {{item.name}}
+            </a>
+        </nav>
+    </section>
+</template>
+
+
+<style scoped>
+    .sidebar-list {
+        padding: 2rem 0;
+    }
+
+    .list-title {
+        padding: 0 2rem;
+        margin-bottom: 0.5rem;
+        font-size: 1rem;
+        text-transform: uppercase;
+    }
+
+    .list-item {
+        display: flex;
+        align-items: center;
+        padding: 0.5rem 2rem;
+    }
+
+    .list-item:hover,
+    .list-item:focus {
+        background-color: var(--panel-primary-highlight-color);
+    }
+
+    .icon {
+        flex-shrink: 0;
+        margin-right: 0.5rem;
+        font-size: 1.6rem;
+    }
+</style>
+
+
+<script>
+    module.exports = {
+        name: 'sidebar-list',
+        props: ['title', 'items']
+    };
+</script>
--- source/sidebar-panel.vue
+++ source/sidebar-panel.vue
@@ -1,6 +1,7 @@
 <template>
     <aside class="sidebar-panel">
         <header-actions></header-actions>
+        <sidebar-list :title="places.title" :items="places.items"></sidebar-list>
     </aside>
 </template>
 
@@ -17,8 +18,23 @@
     module.exports = {
         name: 'sidebar-panel',
 
+        data() {
+            return {
+                places: {
+                    title: 'Places',
+                    items: [
+                        { name: 'Documents', path: '/home/arnelle/Documents' },
+                        { name: 'Downloads', path: '/home/arnelle/Downloads' },
+                        { name: 'Desktop', path: '/home/arnelle/Desktop' },
+                        { name: 'Pictures', path: '/home/arnelle/Pictures' }
+                    ]
+                }
+            };
+        },
+
         components: {
-            'header-actions': require('./header-actions.vue')
+            'header-actions': require('./header-actions.vue'),
+            'sidebar-list': require('./sidebar-list.vue')
         }
     };
 </script>
