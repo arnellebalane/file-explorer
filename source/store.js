aecf26cda2a9e0d3e7046d35e5c0a79594598504
Arnelle Balane <arnellebalane@gmail.com>
2017-03-19 19:01:00 +0800

    store and load current path into localstorage

--- source/store.js
+++ source/store.js
@@ -6,12 +6,13 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
-        path: require('user-home') || '/'
+        path: localStorage.getItem('path') || require('user-home') || '/'
     },
 
     mutations: {
         open(state, path) {
             state.path = path;
+            localStorage.setItem('path', path);
         }
     }
 });
