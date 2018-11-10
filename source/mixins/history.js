9c9f54b10bd3e01988a7fd184e195d0b5818d6c4
Arnelle Balane <arnellebalane@gmail.com>
2018-11-10 12:55:57 +0800

    Dispatch openPath action from history mixin

--- source/mixins/history.js
+++ source/mixins/history.js
@@ -26,13 +26,13 @@ export default {
 
         back() {
             if (this.historyIndex > 0) {
-                this.open(this.history[--this.historyIndex]);
+                this.$store.dispatch('openPath', this.history[--this.historyIndex]);
             }
         },
 
         forward() {
             if (this.historyIndex < this.history.length - 1) {
-                this.open(this.history[++this.historyIndex]);
+                this.$store.dispatch('openPath', this.history[++this.historyIndex]);
             }
         }
     }
